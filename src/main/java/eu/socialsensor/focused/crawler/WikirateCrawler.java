2be659c128c359c3ef1595d5c52d8d2fff2cff21
manosetro <manosetro@iti.gr>
2014-12-10 13:41:25 +0200

    Add crawler

---
+++ src/main/java/eu/socialsensor/focused/crawler/WikirateCrawler.java
@@ -0,0 +1,156 @@
+package eu.socialsensor.focused.crawler;
+
+import java.net.UnknownHostException;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.log4j.Logger;
+
+import eu.socialsensor.focused.crawler.bolts.media.MediaTextIndexerBolt;
+import eu.socialsensor.focused.crawler.bolts.media.MediaUpdaterBolt;
+import eu.socialsensor.focused.crawler.bolts.webpages.ArticleExtractionBolt;
+import eu.socialsensor.focused.crawler.bolts.webpages.MediaExtractionBolt;
+import eu.socialsensor.focused.crawler.bolts.webpages.TextIndexerBolt;
+import eu.socialsensor.focused.crawler.bolts.webpages.URLExpansionBolt;
+import eu.socialsensor.focused.crawler.bolts.webpages.WebPageDeserializationBolt;
+import eu.socialsensor.focused.crawler.bolts.webpages.WebPagesUpdaterBolt;
+import eu.socialsensor.focused.crawler.spouts.RedisSpout;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
+import backtype.storm.generated.StormTopology;
+import backtype.storm.topology.IRichBolt;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichSpout;
+
+public class WikirateCrawler {
+	
+	private static Logger logger = Logger.getLogger(WikirateCrawler.class);
+	
+	public static void main(String[] args) throws UnknownHostException {
+		
+		XMLConfiguration config;
+		try {
+			if(args.length == 1)
+				config = new XMLConfiguration(args[0]);
+			else
+				config = new XMLConfiguration("./conf/wikirate.crawler.xml");
+		}
+		catch(ConfigurationException ex) {
+			logger.error(ex);
+			return;
+		}
+	
+		StormTopology topology = null;
+		try {
+			topology = createTopology(config);
+		}
+		catch(Exception e) {
+			logger.error(e);
+		}
+		
+        // Run topology
+        String name = config.getString("topology.focusedCrawlerName", "WikirateCrawler");
+        boolean local = config.getBoolean("topology.local", true);
+        
+        Config conf = new Config();
+        conf.setDebug(false);
+        
+        if(!local) {
+        	System.out.println("Submit topology to Storm cluster");
+			try {
+				int workers = config.getInt("topology.workers", 4);
+				conf.setNumWorkers(workers);
+				
+				StormSubmitter.submitTopology(name, conf, topology);
+			}
+			catch(NumberFormatException e) {
+				logger.error(e);
+			} catch (AlreadyAliveException e) {
+				logger.error(e);
+			} catch (InvalidTopologyException e) {
+				logger.error(e);
+			}
+			
+		} else {
+			logger.info("Run topology in local mode");
+			LocalCluster cluster = new LocalCluster();
+			cluster.submitTopology(name, conf, topology);
+		}
+	}
+	
+	public static StormTopology createTopology(XMLConfiguration config) {
+	
+		String redisHost = config.getString("redis.hostname", "xxx.xxx.xxx.xxx");
+		String webPagesChannel = config.getString("redis.webPagesChannel", "webpages");
+		
+		String mongodbHostname = config.getString("mongodb.hostname", "xxx.xxx.xxx.xxx");
+		String mediaItemsDB = config.getString("mongodb.mediaItemsDB", "Prototype");
+		String mediaItemsCollection = config.getString("mongodb.mediaItemsCollection", "MediaItems");
+		String streamUsersDB = config.getString("mongodb.streamUsersDB", "Prototype");
+		String streamUsersCollection = config.getString("mongodb.streamUsersCollection", "StreamUsers");
+		String webPagesDB = config.getString("mongodb.webPagesDB", "Prototype");
+		String webPagesCollection = config.getString("mongodb.webPagesCollection", "WebPages");
+		
+		String textIndexHostname = config.getString("textindex.hostname", "xxx.xxx.xxx.xxx:8080/solr");
+		String textIndexCollection = config.getString("textindex.collections.webpages", "WebPages");
+		String mediaIndexCollection = config.getString("textindex.collections.media", "MediaItems");
+		String textIndexService = textIndexHostname + "/" + textIndexCollection;
+		String mediaTextIndexService = textIndexHostname + "/" + mediaIndexCollection;
+		
+		BaseRichSpout wpSpout;
+		IRichBolt wpDeserializer, mediaUpdater, urlExpander, mediaTextIndexer;
+		IRichBolt articleExtraction, mediaExtraction, webPageUpdater, textIndexer;
+		
+		try {
+			wpSpout = new RedisSpout(redisHost, webPagesChannel, "url");
+			wpDeserializer = new WebPageDeserializationBolt(webPagesChannel);
+			urlExpander = new URLExpansionBolt(webPagesChannel);
+			
+			articleExtraction = new ArticleExtractionBolt(24);
+			mediaExtraction = new MediaExtractionBolt();
+			webPageUpdater = new WebPagesUpdaterBolt(mongodbHostname, webPagesDB, webPagesCollection);
+			textIndexer = new TextIndexerBolt(textIndexService);
+
+			mediaUpdater = new MediaUpdaterBolt(mongodbHostname, mediaItemsDB, mediaItemsCollection, streamUsersDB, streamUsersCollection);
+			mediaTextIndexer = new MediaTextIndexerBolt(mediaTextIndexService);
+		} catch (Exception e) {
+			logger.error(e);
+			return null;
+		}
+		
+		// Create topology 
+		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout("wpSpout", wpSpout, 1);
+				
+		builder.setBolt("WpDeserializer", wpDeserializer, 4).shuffleGrouping("wpSpout");
+		builder.setBolt("expander", urlExpander, 8).shuffleGrouping("WpDeserializer");
+				
+				
+		builder.setBolt("articleExtraction", articleExtraction, 1)
+			.shuffleGrouping("expander", "webpage");
+		builder.setBolt("mediaExtraction", mediaExtraction, 1)
+			.shuffleGrouping("expander", "media");
+				
+		builder.setBolt("webPageUpdater", webPageUpdater, 4)
+			.shuffleGrouping("articleExtraction", "webpage")
+			.shuffleGrouping("mediaExtraction", "webpage");
+				
+		builder.setBolt("textIndexer", textIndexer, 1)
+			.shuffleGrouping("articleExtraction", "webpage");
+				
+		builder.setBolt("mediaupdater", mediaUpdater, 1)
+			.shuffleGrouping("articleExtraction", "media")
+			.shuffleGrouping("mediaExtraction", "media");
+		
+		builder.setBolt("mediatextindexer", mediaTextIndexer, 1)
+			.shuffleGrouping("articleExtraction", "media")
+			.shuffleGrouping("mediaExtraction", "media");
+		
+		StormTopology topology = builder.createTopology();
+		return topology;
+	}
+}
